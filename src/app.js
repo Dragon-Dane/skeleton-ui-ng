44a8b0d204dffc22436e09bba4032859f88c4bf1
Matt Warman <warmanmr@gmail.com>
2018-03-01 09:21:30 -0500

    Require Angular. Declare Angular app.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-import './styles/styles.scss';
+const angular = require('angular');
+require('angular-route');
+require('angular-animate');
+require('angular-resource');
+require('angular-sanitize');
+
+require('./styles/styles.scss');
+
+const myAngularApp = angular.module('myAngularApp', ['ngRoute', 'ngAnimate', 'ngResource', 'ngSanitize']);
+
+myAngularApp.controller('ApplicationController', [
+  '$rootScope',
+  '$scope',
+  '$interval',
+  ($rootScope, $scope, $interval) => {
+    console.log('> ApplicationController');
+
+    $scope.currentDate = new Date();
+    const currentDateInterval = $interval(() => {
+      $scope.currentDate = new Date();
+    }, 5000);
+
+    $scope.$on('$destroy', () => {
+      console.log(' destroying ApplicationController scope');
+      if (currentDateInterval) {
+        console.log('  cancelling currentDateInterval');
+        $interval.cancel(currentDateInterval);
+      }
+    });
+
+    console.log('< ApplicationController');
+  }
+]);
