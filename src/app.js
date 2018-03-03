8a891151596bd342606cd4b2f09dfec1749cac83
Matt Warman <warmanmr@gmail.com>
2018-03-03 18:49:32 -0500

    Added FontAwesome 5.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 require('jquery/dist/jquery.js');
 require('popper.js/dist/popper.js');
 require('bootstrap/dist/js/bootstrap.js');
+require('@fortawesome/fontawesome');
+require('@fortawesome/fontawesome-free-solid');
+require('@fortawesome/fontawesome-free-regular');
+require('@fortawesome/fontawesome-free-brands');
 
 require('./app.module');
 require('./app.config');
